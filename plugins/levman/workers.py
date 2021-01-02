82ea43fe08fd9b653f4fbc0c8e335ca66a5949ae
dmattie <dmattie@stfx.ca>
2021-01-02 19:19:29 -0400

    cleanup cache files - taking too much space on scratch

--- plugins/levman/workers.py
+++ plugins/levman/workers.py
@@ -159,8 +159,11 @@ class workerTrackvis(object):
             datafile = "%s/crush/%s/%s-%s-%s.nii.txt" %(tractographypath,segment,segment,counterpart,method)
             oldcalcsfile = "%s/crush/calcs-%s-%s-%s.json" %(tractographypath,segment,counterpart,method)
                     
-            #if os.path.isfile(nii):
-            #    os.unlink(nii) 
+            if os.path.isfile(nii):
+                os.unlink(nii) 
+            
+            if os.path.isfile(datafile):
+                os.unlink(datafile)
                     
             if os.path.isfile(oldcalcsfile):
                 print("Cleanup %s" %(oldcalcsfile))
