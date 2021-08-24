f732f308a3ad21ff10de8f9126274ad3bfd2c44f
dmattie <dmattie@stfx.ca>
2021-08-24 18:47:32 -0300

    adnie convert to BIDS stage 0

--- utilities/contrib/adni/initiate.sh
+++ utilities/contrib/adni/initiate.sh
@@ -12,8 +12,9 @@ fsplice() {
 }
 
 PATIENT_QUEUE=~/projects/def-dmattie/data/adni-queue
-ROOT_DIR=~/projects/rrg-jlevman/shared/adni
-SUBJECTS_DIR=$ROOT_DIR/stage_0
+ROOT_DIR=~/projects/rrg-jlevman/shared/adni/dataset
+SUBJECTS_DIR=$ROOT_DIR/rawdata/stage_0
+SOURCE_DIR=$ROOT_DIR/source
 util=$(pwd)
 
 
@@ -55,11 +56,11 @@ fi
 echo "Initiating patient: $patientID"
 
 echo "****START************"
-if [ ! -d "$ROOT_DIR/stage_0/$patientID" ] &&
-[ ! -d "$ROOT_DIR/stage_1/$patientID" ] &&
-[ ! -d "$ROOT_DIR/stage_2/$patientID" ] &&
-[ ! -d "$ROOT_DIR/stage_3/$patientID" ] &&
-[ ! -d "$ROOT_DIR/stage_4/$patientID" ] ; then
+if [ ! -d "$ROOT_DIR/rawdata/stage_0/$patientID" ] &&
+[ ! -d "$ROOT_DIR/rawdata/stage_1/$patientID" ] &&
+[ ! -d "$ROOT_DIR/rawdata/stage_2/$patientID" ] &&
+[ ! -d "$ROOT_DIR/rawdata/stage_3/$patientID" ] &&
+[ ! -d "$ROOT_DIR/rawdata/stage_4/$patientID" ] ; then
   # Looks like we haven't seen this patient yet    
     cat $util/cascade_raw_to_0.sh.template | sed -e "s/PATIENT_RAW_0/$patientID/" > ~/jobs/generated/$patientID-raw_0.sh    
     chmod u+x ~/jobs/generated/$patientID-raw_0.sh
@@ -70,26 +71,26 @@ if [ ! -d "$ROOT_DIR/stage_0/$patientID" ] &&
    # sbatch ~/jobs/generated/$patientID-raw_0.sh 
     echo "*****END RUN raw-0********"
 else
-    if [ -d "$ROOT_DIR/stage_0/$patientID" ]; then
+    if [ -d "$ROOT_DIR/rawdata/stage_0/$patientID" ]; then
         echo "THIS SAMPLE ALREADY EXISTS IN THE PIPELINE, STAGE 0"
         echo "Recommended next step: ~/jobs/generated/$patientID-0_1.sh"
     fi
 
-    if [ -d "$ROOT_DIR/stage_1/$patientID" ]; then
+    if [ -d "$ROOT_DIR/rawdata/stage_1/$patientID" ]; then
         echo "THIS SAMPLE ALREADY EXISTS IN THE PIPELINE, STAGE 1"
         echo "Recommended next step: ~/jobs/generated/$patientID-1_2.sh"
     fi
 
-    if [ -d "$ROOT_DIR/stage_2/$patientID" ]; then
+    if [ -d "$ROOT_DIR/rawdata/stage_2/$patientID" ]; then
         echo "THIS SAMPLE ALREADY EXISTS IN THE PIPELINE, STAGE 2"
         echo "Recommended next step: ~/jobs/generated/$patientID-2_3.sh"
     fi
 
-    if [ -d "$ROOT_DIR/stage_3/$patientID" ]; then
+    if [ -d "$ROOT_DIR/rawdata/stage_3/$patientID" ]; then
         echo "THIS SAMPLE ALREADY EXISTS IN THE PIPELINE, STAGE 3"
     fi
     
-    if [ -d "$ROOT_DIR/stage_4/$patientID" ]; then
+    if [ -d "$ROOT_DIR/rawdata/stage_4/$patientID" ]; then
         echo "THIS SAMPLE ALREADY EXISTS IN THE PIPELINE, STAGE 4"
     fi
 fi
